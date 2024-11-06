61ed751ecf58eec3d6013458c233c97c5a5f4fe4
nikachitava <chitava18.official@gmail.com>
2024-11-06 22:17:59 +0400

    configure vercel.json

---
+++ api/index.ts
@@ -0,0 +1,20 @@
+// api/index.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { Handler, Context, Callback, APIGatewayEvent } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
+
+let server: Handler;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
