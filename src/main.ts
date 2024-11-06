8eefd51fc6d26fce955c4fa1f52b356e8fa8247d
nikachitava <chitava18.official@gmail.com>
2024-11-06 21:43:50 +0400

    change origin url for cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
-    origin: "http://localhost:5173",
+    origin: "https://your-auto-client.vercel.app/",
     methods: 'GET, POST, DELETE, PATCH',
     credentials: true
 
