6b8f5553bf2ef3e2f70ad9d3b49a90a9fb5e8416
nikachitava <chitava18.official@gmail.com>
2025-02-26 22:37:20 +0400

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import { join } from 'path';
-import * as express from 'express';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
-    // origin: 'https://your-auto-client.vercel.app',
-    origin: 'http://localhost:5173',
+    origin: 'https://your-auto-client.vercel.app',
+    // origin: 'http://localhost:5173',
     methods: 'GET, POST, DELETE, PATCH',
     credentials: true,
 
